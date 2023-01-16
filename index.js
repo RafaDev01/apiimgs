91d8d65b2f8519fa9b2476a4165d69b4a9d01cf5
Rafael Nascimento <rflwwe123@hotmail.com>
2023-01-16 15:19:07 -0300

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+const imgs = require('./src/data/img.json')
+
+server.get('/imgs',(req,res) =>{
+    return res.json(imgs)
+})
+
+server.listen(3000, ()=>{
+    console.log("Funciona")
+});
